f75d6757b8a4465e07ce58701d5329b7fe4b9fdf
Kuba <jakub.cichorski@hotmail.com>
2020-09-04 14:25:12 +0200

    VideoCard component done

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../css/Results.css';
 import VideoCard from "./ViedoCard";
+import axios from '../axios';
+import requests from '../requests';
 
 function Results() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            let response = await axios.get(requests.
+                fetchActionMovies);
+            setMovies(response.data.results);
+            return response
+        })();
+    }, []);
+
     return (
         <div className="results">
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
+            {movies.map((movie, index) => (
+                <VideoCard key={index} movie={movie} />
+            ))}
         </div>
     )
 }
--- src/components/ViedoCard.js
+++ src/components/ViedoCard.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import '../css/VideoCard.css';
+import {ThumbUpSharp} from "@material-ui/icons";
+
+const base_url = 'https://image.tmdb.org/t/p/original/';
 
 function VideoCard({movie}) {
+    console.log(movie);
     return (
         <div className="video__card">
-            <img src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png" alt=""/>
-            <p>Movie about movie</p>
-            <h2>Movie Title</h2>
-            <p>No of likes..</p>
+            <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="movie poster"/>
+            <p>{movie.overview}</p>
+            <h2>{movie.title || movie.original_title}</h2>
+            <p className="movie__likes">
+                {movie.media_type && `${movie.media_type} &#8226;`}
+                {movie.release_date || movie.first_air_date} &#8226;
+                <ThumbUpSharp/>
+                {movie.vote_count}
+            </p>
         </div>
     )
 }
