28fc90f7e5ade5507d35d9db4283b9359884dbff
Kuba <jakub.cichorski@hotmail.com>
2020-09-07 23:44:53 +0200

    MoreInfo cards now contain a link to watch movie trailer

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/MoreInfo.css';
 import closeIcon from '../icons/close.svg';
+import youtubeLogo from '../icons/youtube-logo.svg';
 import {useStateValue} from "../store/StateProvider";
 import {CLEAR_MOREINFO} from '../store/reducer';
 import {ThumbUpSharp} from "@material-ui/icons";
@@ -15,6 +16,9 @@ function MoreInfo({movie}) {
         }
         dispatch(action);
     }
+    const title = movie.title || movie.original_title || movie.name;
+    const date = movie.release_date || movie.first_air_date;
+    const trailerRedirect = `https://www.youtube.com/results?search_query=${title} ${date.slice(0, 4)} trailer`;
 
     return (
         <div className="more__info">
@@ -22,12 +26,15 @@ function MoreInfo({movie}) {
                 <img src={closeIcon} alt={"close icon"} className={"close__icon"} onClick={() => hideInfo()}/>
                 <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="movie poster" className={"movie__poster"}/>
                 <p>{movie.overview}</p>
-                <h2>{movie.title || movie.original_title || movie.name}</h2>
-                <p className="">
-                    {movie.release_date || movie.first_air_date} <span className="dot">&#8226;</span>
+                <h2>{title}</h2>
+                <p className="movie__info">
+                    {date} <span className="dot">&#8226;</span>
                     <ThumbUpSharp/>
                     {movie.vote_count} <span className="dot">&#8226;</span>
                     {movie.vote_average} / 10
+                    <a href={trailerRedirect} target="_blank" rel="noopener noreferrer">
+                        <img src={youtubeLogo} alt="YouTube logo"  className={"yt__logo"}/>
+                    </a>
                 </p>
             </div>
         </div>
